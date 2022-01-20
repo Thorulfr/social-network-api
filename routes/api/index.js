fb139f0b29b1ccfecc80a528b86714b560245396
Ben H <deepfreespace@gmail.com>
2022-01-20 16:18:37 -0700

    Starting controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-// const commentRoutes = require('./comment-routes');
-// const pizzaRoutes = require('./pizza-routes');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-// router.use('/comments', commentRoutes);
-// router.use('/pizzas', pizzaRoutes);
-
+// Prefix routes
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 module.exports = router;
