8391d756313e250b50f09073b574d90920f64bf8
Ben H <deepfreespace@gmail.com>
2022-01-20 16:15:04 -0700

    Route skeletons

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// Imports
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// Prefix /api to api routes
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error!</h1>');
+});
+
+module.exports = router;
