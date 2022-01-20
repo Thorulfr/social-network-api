97063cbadf2d567fe1c78c33e7d48c527704a330
Ben H <deepfreespace@gmail.com>
2022-01-20 16:58:04 -0700

    Get all users

---
+++ controllers/thought-controller.js
@@ -0,0 +1,27 @@
+// Imports
+const { User } = require('../models');
+
+const userController = {
+    // Get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'users',
+                select: '-__v',
+            })
+            .select('__v')
+            .sort({ _id: -1 })
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => {
+                console.error(err);
+                res.status(400).json(err);
+            });
+    },
+    // Get one user by ID with thought/friend data
+    // Create a new user
+    // Update a user by ID
+    // Delete a user by ID
+    // Bonus: Delete associated thoughts on user deletion
+    // Add a friend to a user's friend list using both IDs
+    // Delete a friend from a user's friend list using both IDs
+};
